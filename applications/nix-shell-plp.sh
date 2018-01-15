d41a4c3b7498dd089a416809b43be92b00350106
Tim Docker <tim@dockerz.net>
2018-01-15 23:34:20 +1100

    cd to polez repo in plp terminal

--- applications/nix-shell-plp.sh
+++ applications/nix-shell-plp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo -ne "\033]0;nix-shell-plp\007"
-exec nix-shell --command "source $HOME/.bash_prompt; return" $HOME/nix/plp.nix
+exec nix-shell --command "source $HOME/.bash_prompt; cd $HOME/helix/projects/polez/polez; return" $HOME/nix/plp.nix
